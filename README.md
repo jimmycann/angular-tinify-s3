acd368b46d70280d374a16e92270daaee63ce22a
Jimmy Cann <mail@jimmycann.com>
2016-04-03 14:44:08 +1000

    Bring code in line with tutorial, update readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # angular-tinify-s3
+
+Dependencies
+
+NodeJS, Bower
+
+Quick Start
+
+cd angular-tinify-s3
+npm install
+cd public
+bower install
+
+Jump into your favourite editor and change the following parameters in /app/routes.js
+
+YOUR_TINIFY_API_KEY, YOUR_AWS_ACCESS_KEY_ID, YOUR_AWS_SECRET_ACCESS_KEY, your-bucket-region, your-bucket-name
+
+cd ../
+nodemon server.js
+
+Tutorial available at 
--- public/js/controller/upload-controller.js
+++ public/js/controller/upload-controller.js
@@ -1,33 +1,37 @@
 angular.module('angular-tinify-s3').controller('uploadCtrl',['$scope', '$window', 'Upload', 
     function($scope, $window, Upload){
-    $scope.ulData={};
-    $scope.ulData.ht=150;
-    $scope.ulData.wd=150;
-    $scope.ulData.m='fit';
 
-    $scope.method = [
-        { value: 'scale', name: 'Scale' },
-        { value: 'fit', name: 'Fit' },
-        { value: 'cover', name: 'Cover' }
-    ];
+        // Set some model defaults for the Tinify API Parameters
+        $scope.ulData={};
+        $scope.ulData.ht=150;
+        $scope.ulData.wd=150;
+        $scope.ulData.m='fit';
 
-    var vm = this;
-    vm.submit = function(){
-        if (vm.upload_form.file.$valid && vm.file) {
-            vm.upload(vm.file, $scope.ulData);
-        }
-    };
+        // Resize methods accepted (for the select box options)
+        $scope.method = [
+            { value: 'scale', name: 'Scale' },
+            { value: 'fit', name: 'Fit' },
+            { value: 'cover', name: 'Cover' }
+        ];
 
-    vm.upload = function (file, ulData) {
-        Upload.upload({
-            url: '/api/upload',
-            data:{
-                file:file,
-                ulData:ulData
+        //Angular File Upload functions
+        var vm = this;
+        vm.submit = function(){
+            if (vm.upload_form.file.$valid && vm.file) {
+                vm.upload(vm.file, $scope.ulData);
             }
-        }).then(function() {
-            alert('Upload Complete!');
-        });
-    };
+        };
+
+        vm.upload = function (file, ulData) {
+            Upload.upload({
+                url: '/api/upload',
+                data:{
+                    file:file,
+                    ulData:ulData
+                }
+            }).then(function() {
+                alert('Upload Complete!');
+            });
+        };
 
 }]);
